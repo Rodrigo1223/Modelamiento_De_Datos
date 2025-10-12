adc7a3cd6f25345f1a75554b0f355d4b284d263e
Rodrigo1223 <rod.olavarria@duocuc.cl>
2025-10-12 00:28:55 -0300

    EFT_S9_Base_de_Datos

---
+++ PRY2204_S9~1.sql
@@ -0,0 +1,154 @@
+
+Table ASIGNACION_TURNO borrado.
+
+
+Table ORDEN_MANTENCION borrado.
+
+
+Table MAQUINA borrado.
+
+
+Table TECNICO_MANTENCION borrado.
+
+
+Table JEFE_TURNO borrado.
+
+
+Table OPERARIO borrado.
+
+
+Table EMPLEADO borrado.
+
+
+Table PLANTA borrado.
+
+
+Table TURNO borrado.
+
+
+Table TIPO_MAQUINA borrado.
+
+
+Table AFP borrado.
+
+
+Table SISTEMA_SALUD borrado.
+
+
+Table AFP creado.
+
+
+Table SISTEMA_SALUD creado.
+
+
+Table PLANTA creado.
+
+
+Table TIPO_MAQUINA creado.
+
+
+Table TURNO creado.
+
+
+Table EMPLEADO creado.
+
+
+Table OPERARIO creado.
+
+
+Table JEFE_TURNO creado.
+
+
+Table TECNICO_MANTENCION creado.
+
+
+Table MAQUINA creado.
+
+
+Table ORDEN_MANTENCION creado.
+
+
+Table ASIGNACION_TURNO creado.
+
+
+1 fila insertadas.
+
+
+1 fila insertadas.
+
+
+1 fila insertadas.
+
+
+1 fila insertadas.
+
+
+1 fila insertadas.
+
+
+1 fila insertadas.
+
+
+1 fila insertadas.
+
+
+1 fila insertadas.
+
+
+1 fila insertadas.
+
+
+1 fila insertadas.
+
+
+1 fila insertadas.
+
+
+1 fila insertadas.
+
+
+1 fila insertadas.
+
+
+1 fila insertadas.
+
+
+1 fila insertadas.
+
+
+1 fila insertadas.
+
+
+1 fila insertadas.
+
+
+1 fila insertadas.
+
+
+1 fila insertadas.
+
+
+1 fila insertadas.
+
+
+1 fila insertadas.
+
+
+1 fila insertadas.
+
+
+1 fila insertadas.
+
+
+1 fila insertadas.
+
+
+1 fila insertadas.
+
+Confirmación terminada.
+
+EMPLEADO                                                                                                                                                                                                  ROL_PRINCIPAL      ROL_ASIGNADO                                       NOMBRE_TURNO                                       MAQUINA_ASIGNADA                                                                                     NOMBRE_PLANTA                                                                                       
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- ------------------ -------------------------------------------------- -------------------------------------------------- ---------------------------------------------------------------------------------------------------- ----------------------------------------------------------------------------------------------------
+Ana Rojas                                                                                                                                                                                                 OPERARIO           Operador Principal                                 Mañana                                             CT-01                                                                                                Planta Norte                                                                                        
+Pedro Mendez                                                                                                                                                                                              JEFE_TURNO         Supervisor de Línea                                Mañana                                             CT-01                                                                                                Planta Norte                                                                                        
+
